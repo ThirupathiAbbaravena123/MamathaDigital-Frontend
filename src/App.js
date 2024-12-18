d280f82bef12e778324c27b4364a12e92a821136
Tatasriram9 <sriram9.dev@gmail.com>
2024-12-18 15:51:53 +0530

    dashboard was modified

--- src/App.js
+++ src/App.js
@@ -37,24 +37,6 @@ const App = () => {
 
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
-
-  const [windowSize, setWindowSize] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
-  });
-
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
-    };
-
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
   }, []);
 
   const renderContent = () => {
